73d068293ad8d58964f5a89366f27c8fbab557d4
chauquach1 <chau268@gmail.com>
2023-07-07 22:50:29 -0700

    added js functions and event listeners to take in user input from user keyboard, gave each DOM keyboard key a 'click' event listener, able to type in each tile, and css edits

--- js/app.js
+++ js/app.js
@@ -1,3 +1,25 @@
+let gameProgress = {
+  corrWord: '',
+  grid: [
+    ['','','','',''], 
+    ['','','','',''], 
+    ['','','','',''], 
+    ['','','','',''], 
+    ['','','','',''], 
+    ['','','','','']
+  ],
+  row: 0,
+  col: 0
+}
+
+function startup () {
+  generateKeys()
+  generateGrid()
+  keyboardLister ()
+}
+startup()
+
+
 // create a function that takes in parameters for specific grid coordinates
   // "row" argument will be assigned via loop
   // "col" aka "column" will be assigned via loop
@@ -35,6 +57,7 @@ function generateGrid() {
   // each row will contain a specified number of characters
   // create a for loop that iterates each nested array (row of characters)
     // create a div for each key within that current row
+    // give each div the ability to act as a virtual keyboard using event listener
 function generateKeys () {
   const keyboard = [
     ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
@@ -54,18 +77,78 @@ function generateKeys () {
       characterKey.setAttribute('class', 'char-key');
       characterKey.id = character + " Key"
       characterKey.innerText = character;
-      // characterKey.addEventListener('click', ) // <-- revisit to add function on 'click'
+      // Add 'click' functionality to DOM keyboard
+      characterKey.addEventListener('click', (input) => {
+        input.key = character
+        isCharKey(input)
+      })
       keyboardRow.append(characterKey)
     }
   }
 }
 
-
-
-
-function generateGame () {
-  generateKeys()
-  generateGrid()
+// Check if keyboard input is a valid character
+function isCharKey (key) {
+  // key.length is used because the 'key' property can return "Enter", "Escape", "Space", etc.
+  // key.match uses a regexp that matches the 'key' value to an letter within the range 'a' to 'z' while ignoring case (upper or lower) using the 'i' flag
+  // isCharKey() will return false if any other input is pressed
+      // console.log(key); <-- Used to check if input is being read
+    return (key.length === 1 && key.match(/[a-z]/i));
 }
 
-generateGame()
+
+
+
+
+
+
+function keyboardLister () {
+  let row = 0;
+  let col = 0;
+  let currRow = gameProgress.row;
+  let currCol = gameProgress.col;
+  let currGuess = '';
+  document.addEventListener('keydown', (input) => {
+    let currTile = document.getElementById(`tile${row}${col}`);
+      if (!isCharKey(input.key)) {
+        if (input.key == 'Enter'){
+          console.log(currGuess);
+          col = 0;
+          row ++;
+          // return // <-- delete when submitAnswer() function declared
+        } else if (input.key == 'Backspace') {
+          // deleteCharacter();
+          return //<-- delete when deleteCharacter() function declared
+        }
+      } else if (col <= 5 && currTile) {
+          currTile.textContent = input.key;
+          currGuess.concat(input.key)
+                        // logs current tile positioning is accurate and checks if conditionals are true
+                          console.log('currTile: ', currTile);
+                          console.log('row: ' + row);
+                          console.log('col: ' + col);
+                          console.log('currRow: ' + currRow);
+                          console.log('currCol: ' + currCol);
+                          console.log('input key: ', input.key)
+          col ++;
+          currCol ++;
+      }
+    })
+  }
+
+
+// create guessInput()
+  // this function would be a callback function that allows the user to use both the DOM keyboard and user's physical keyboard to type, submit and edit their currGuess
+
+
+
+// Create a submitGuess() that checks the currGuess with: guessList
+  // if currGuess is present in guessList, then...
+  // check for all present characters in currGuess that are in the chosenWordle
+    // create a loop that cross references each present character's index with the character indexes of the chosenWordle
+      // if the currGuess character and it's index is === to those of the chosenWordle
+        // turn the tile "green"
+      // else if the currGuess character is present but not in the same position as that of the chosenWordle
+        //turn the tile "yello"
+      // else turn the tile "grey"
+    // increase the currRow value by 1 and reset the currCol value back to 0
