150efd1352545b9288088ed9dd24e2c80c68096d
chauquach1 <chau268@gmail.com>
2023-07-08 18:18:04 -0700

    added guess check function, added update row ability, enter key invokes guess check function

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 let gameProgress = {
-  corrWord: '',
+  wordle: 'blast',
   grid: [
     ['','','','',''], 
     ['','','','',''], 
@@ -63,88 +63,113 @@ function generateKeys () {
     ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
     ["A", "S", "D", "F", "G", "H", "J", "K", "L",],
     ["Z", "X", "C", "V", "B", "N", "M",]
-  ]
+  ];
+
   for (let i = 0; i < keyboard.length; i++) {
     const keyboardContainer = document.getElementById('keyboard-keys-container')
     const row = keyboard[i];
-    const keyboardRow = document.createElement('div')
-    keyboardRow.setAttribute('class', 'keyboard-row')
-    keyboardContainer.appendChild(keyboardRow)
+    const keyboardRow = document.createElement('div');
+    keyboardRow.setAttribute('class', 'keyboard-row');
+    keyboardContainer.appendChild(keyboardRow);
     
     for (let j = 0; j < row.length; j++) {
       const character = row[j];
       const characterKey = document.createElement('div');
       characterKey.setAttribute('class', 'char-key');
-      characterKey.id = character + " Key"
+      characterKey.id = character + " Key";
       characterKey.innerText = character;
       // Add 'click' functionality to DOM keyboard
       characterKey.addEventListener('click', (input) => {
-        input.key = character
+        input.key = character.toLowerCase();
         if(isCharKey(input.key)) {
-          characterInputLister(input.key)
+          keyInputLister(input.key);
         }
       })
-      keyboardRow.append(characterKey)
+      keyboardRow.append(characterKey);
     }
   }
 }
 
-// Check if keyboard input is a valid character
-function isCharKey (input) {
-  // input.length is used because the 'key' property from eventListener can return "Enter", "Escape", "Space", etc.
-  // input.match uses a regexp that matches the 'key' value from eventListener to letter within the range 'a' to 'z' while ignoring case (upper or lower) using the 'i' flag
-  // isCharKey() will return false if any other input is pressed
-            // console.log(input); // <-- Used to check if input is being read
-    return input.length === 1 && input.match(/[a-z]/i);
-}
 
-// Allow for user input via physical keyboard presses
+// Check if keyboard input is a valid character and will return false if any other input is pressed
+function isCharKey (input) {
+  return (input.length === 1 && input.match(/[a-z]/i));
+      // input.length is used because the 'key' property from eventListener can return multicharacter-inputs like "Enter", "Escape", "Space", etc.
+      // input.match uses a regexp that matches the 'key' value from eventListener to letter within the range 'a' to 'z' while ignoring case (upper or lower) using the 'i' flag
+};
+
+// DOM event listener for user input via physical keyboard presses
 document.addEventListener('keydown', (input) => {
   if (isCharKey(input.key)) {
-      characterInputLister(input.key.toLowerCase())
-  } else if (input.key == 'Enter'){
-      // submitGuess(input.key)
-      console.log('Enter key: ', input.key.toLowerCase())
-  } else if (input.key == 'Backspace') {
-      // deleteCharacter(input.key);
-      console.log('Delete key: ', input.key)
+    keyInputLister(input.key.toLowerCase());
+  } else if (input.key === 'Enter') {
+    checkGuess();
+  } else if (input.key === 'Backspace') {
+    // deleteCharacter();
+    console.log(input.key)
   }
-})
+});
 
 
 
-
-function characterInputLister (input) {
+function keyInputLister (input) {
   let currRow = gameProgress.currRow;
   let currCol = gameProgress.currCol;
   let currTile = document.getElementById(`tile${currRow}${currCol}`);
-  
-  if (currCol <= 5 && currTile) {
-    currTile.textContent = input;
-    gameProgress.currGuess += input;
-    gameProgress.currCol += 1;
-      // useful console.logs
-          console.log('input key: ', input)  // <-- logs all inputs (DOM keyboard and user's keyboard)
-          // console.log('currGuess: ', gameProgress.currGuess); // <-- tracks user's current guess attempt
-          // console.log('currTile: ', currTile); // <-- tracks current tile being affected
-          // console.log('currRow: ' + currRow); // <-- used as a cross reference with tile.id
-          // console.log('currCol: ' + currCol); //  <-- used as a cross reference with tile.id
-    }
+  if ((currCol >= 0 && currCol <= 4) && currTile) {
+    currTile.textContent = input; // <-- set's tile character
+    gameProgress.currGuess += input; // <-- updates current guess per character input
+    gameProgress.currCol += 1; // <-- after each input, go to next tile
+  };
+
+  // useful console.logs
+    // console.log('input key: ', input)  // <-- logs all character inputs (DOM keyboard and user's keyboard)
+    // console.log('currCol: ' + currCol); //  <-- used as a cross reference with tile.id (place before currCol += 1)
+    // console.log('currRow: ' + currRow); // <-- used as a cross reference with tile.id (place before currCol += 1)
+    // console.log('currGuess: ', gameProgress.currGuess); // <-- tracks user's current guess attempt
+    // console.log('currTile: ', currTile); // <-- tracks current tile being affected (place before currCol += 1)
+
 }
 
 
-// create guessInput()
-  // this function would be a callback function that allows the user to use both the DOM keyboard and user's physical keyboard to type, submit and edit their currGuess
 
+function checkGuess () {
+  if (gameProgress.currGuess.length === 5) {
+    if (wordList.includes(gameProgress.currGuess) || guessList.includes(gameProgress.currGuess)) {
+      // console.log('guess present in either list: ', gameProgress.currGuess);
+      if (gameProgress.currGuess === gameProgress.wordle) {
+        console.log('correct guess: ', gameProgress.currGuess);
+        showGuess() 
+          // checks each character, its index and adds .correct class and animation to each tile
+        // newGame()
+          // updates game stats
+          // resets tiles to default, changes wordle, and resets currCol/currRow to 0
+      } else {
+          console.log('incorrect guess: ', gameProgress.currGuess);
+          showGuess()
+          nextRow();
+      }
 
+    }
+  }
 
-// Create a submitGuess() that checks the currGuess with: guessList
-  // if currGuess is present in guessList, then...
-  // check for all present characters in currGuess that are in the chosenWordle
-    // create a loop that cross references each present character's index with the character indexes of the chosenWordle
-      // if the currGuess character and it's index is === to those of the chosenWordle
-        // turn the tile "green"
-      // else if the currGuess character is present but not in the same position as that of the chosenWordle
-        //turn the tile "yello"
-      // else turn the tile "grey"
-    // increase the currRow value by 1 and reset the currCol value back to 0
+  function nextRow () {
+    gameProgress.currRow++;
+    gameProgress.currCol = 0;
+    gameProgress.currGuess = '';
+  }
+
+  // showguess() checks each character, its index and adds .correct class and animation to each tile
+    // check for all present characters in currGuess that are in the chosenWordle
+      // create a loop that cross references each present character's index with the character indexes of the chosenWordle
+        // if the currGuess character and it's index is === to those of the chosenWordle
+          // turn the tile "green"
+        // else if the currGuess character is present but not in the same position as that of the chosenWordle
+          //turn the tile "yellow"
+        // else turn the tile "grey"
+  function showGuess () {
+    console.log('**guess animation happening**');
+  }
+
+}
+
